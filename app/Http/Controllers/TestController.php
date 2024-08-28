762b92eca2e1228507d9fa84774f1ab399ff46ba
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-08-28 18:00:36 +0900

    sec1 learning

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Test;
+
+class TestController extends Controller
+{
+    public function index() {
+
+        // Eloquent
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb')->get();
+
+        // クエリビルダー
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
+
+        return view('tests.test', compact('values'));
+    }
+}
