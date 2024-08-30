c0216d83912d916c0cad802c2dd3c36439fb15b2
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-08-30 14:52:56 +0900

    creating restfull_app

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,6 +10,8 @@ class TestController extends Controller
 {
     public function index() {
 
+        dd('test');
+
         // Eloquent
         $values = Test::all();
 
--- resources/views/tests/test.blade.php
+++ resources/views/tests/test.blade.php
@@ -2,5 +2,5 @@ test<br>
 
 @foreach ($values as $value)
 {{ $value->id }}<br>
-{{ $value->text }}<br>
+{{ $value->test }}<br>
 @endforeach
